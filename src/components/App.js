529325ec2fea418d83af6ab8d77e5b3772c28e8d
Kevin Bravo <kbravo1024@gmail.com>
2023-05-19 17:52:08 -0700

    add components

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,8 @@ console.log(blogData);
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      {/* You're on your own from here! Follow the deliverables; test things out in
+      the browser as you write your code; and good luck! */}
     </div>
   );
 }
